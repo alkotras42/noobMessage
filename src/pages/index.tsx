6ae473026b41822bd1457cea664ca06a57992e8e
alkotras42 <kirill.stogniy@mail.ru>
2023-02-06 20:46:34 +0300

    creating user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import {signIn, signOut, useSession} from 'next-auth/react';
+
 export default function Home() {
-  return <div>Hello home page</div>;
+  const {data, status} = useSession();
+
+  return (
+    <div>
+      {data?.user?.name ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn('google')}>Sign In</button>
+      )}
+
+      {data?.user?.name}
+    </div>
+  );
 }
