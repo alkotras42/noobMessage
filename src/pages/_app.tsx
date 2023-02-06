aa2c49465c73a6720635ab929573e4dc90c5be8e
alkotras42 <kirill.stogniy@mail.ru>
2023-02-06 21:13:51 +0300

    setup chakra ui with dark theme as default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import {SessionProvider} from 'next-auth/react';
 import type {AppProps} from 'next/app';
+import {ChakraProvider} from '@chakra-ui/react';
+import {theme} from '../chakra/theme';
 
 export default function App({Component, pageProps}: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme} cssVarsRoot="body">
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 }
